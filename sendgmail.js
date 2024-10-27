d8dc992aa9daf017acb9b28b0bb14ef1d4adeb01
Alejandro López <woloqohola@gmail.com>
2024-10-27 11:25:24 -0700

    cambio 180 grados

---
+++ sendgmail.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_9eb98mt';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+
+    document.getElementById("from_name").value = "";
+    document.getElementById("message").value = "";
+    document.getElementById("email_id").value = "";
+
+});
